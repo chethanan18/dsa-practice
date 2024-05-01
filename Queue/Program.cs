688a827e171b8cd595e11dcc67642e6729363d46
Chethana <chethana.n18@gmail.com>
2024-04-30 23:38:01 -0700

    Initial Version

---
+++ Queue/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class QueueDataStructure
+{
+    public static void Main()
+    {
+        //Queue is FIFO - First In First Out
+        Queue<int> queue = new Queue<int>();
+
+        //Enqueue Operation
+        queue.Enqueue(1);
+        queue.Enqueue(2);
+        queue.Enqueue(3);
+        queue.Enqueue(4);
+        Console.WriteLine("Enqueue Operation:");
+        foreach(var item in queue)
+        Console.WriteLine(item);
+
+        //Dequeue Operation
+        queue.Dequeue();
+        Console.WriteLine("Dequeue Operation:");
+        foreach(var item in queue)
+        Console.WriteLine(item);
+        
+    }
+}
---
+++ Stack/Program.cs
@@ -0,0 +1,33 @@
+﻿using System; 
+using System.Collections.Generic;
+
+public class StackDataStructure
+{
+    public static void Main()
+    {
+        //Stack is LIFO that is Last In First Out
+        //Stores data into vertical structure
+        //Push() to add to the top
+        //Pop() to remove from the top
+        Stack<string> stk = new Stack<string>();
+
+        //Add names to stack
+        stk.Push("John");
+        stk.Push("Marie");
+        stk.Push("Raj");
+        stk.Push("Robert");
+
+        //Print stack items
+        Console.WriteLine("Before deletion");
+        foreach(var names in stk)
+        Console.WriteLine(names);
+        
+        //Remove the top item
+        stk.Pop();
+
+        //Print stack items
+        Console.WriteLine("After deletion");
+        foreach(var names in stk)
+        Console.WriteLine(names);
+    }
+}
